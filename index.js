9e2f901ffc5574992d3bf725865acf9de938beb0
lazer <ajlazer002@gmail.com>
2024-09-13 21:28:32 +0530

    few db changes

--- index.js
+++ index.js
@@ -12,19 +12,23 @@ app.use(express.json())
 app.use("/static",express.static('public'))
 app.use("/static",express.static("upload"))
 app.use(bodyparser.urlencoded({extended:true}))
+const RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+
 app.set('trust proxy', 1);
 
+const redisClient = redis.createClient();
+
 app.use(session({
-cookie:{
-    secure: true,
-    maxAge:600000
-       },
-store: new RedisStore(),
-secret: 'gjiangkwenighwaigkwangiwhigwaigknwik',
-saveUninitialized: true,
-resave: false
+  store: new RedisStore({ client: redisClient }),
+  secret: 'gjiangkwenighwaigkwangiwhigwaigknwik',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: true, // Set this to true only if using HTTPS
+    maxAge: 600000
+  }
 }));
- 
 
 
 
