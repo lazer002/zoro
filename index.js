c96cbafcb55cfebaaa26253d80e80a9e67a315c1
lazer <ajlazer002@gmail.com>
2024-09-13 21:22:03 +0530

    few db changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const bodyparser = require('body-parser')
 
 const router=  require('./src/routes/route.js')
-const session = require('express-session')
+// const session = require('express-session')
+const session = require('cookie-session')
 const path = require('path')
 const cookie=require('cookie-parser')
 app.set('view engine','ejs')
@@ -11,17 +12,18 @@ app.use(express.json())
 app.use("/static",express.static('public'))
 app.use("/static",express.static("upload"))
 app.use(bodyparser.urlencoded({extended:true}))
-app.use(
-    session({
-       
-        secret:'iamdevagooddev',
-        resave:false,
-        saveUninitialized:false,
-        cookie:{
-              maxAge:2000000000
-        }
-    })
- )
+app.set('trust proxy', 1);
+
+app.use(session({
+cookie:{
+    secure: true,
+    maxAge:600000
+       },
+store: new RedisStore(),
+secret: 'gjiangkwenighwaigkwangiwhigwaigknwik',
+saveUninitialized: true,
+resave: false
+}));
  
 
 
